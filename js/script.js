49600dc5e4685090f15f75e41ff6e193c594337c
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-03-04 04:19:50 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@
 // Created by: Jakub Malhotra
 // Created on: Febuary 2023
 // This file contains the JS functions for index.html
-;<script>function myButtonClicked() {alert("Hello, Wordl!")}</script>
+
+function myButtonClicked() {
+  alert("Hello, World!")
+}
