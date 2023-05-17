6a8432394c27605cb2a33ca297cc0e8fdd60a591
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-05-17 17:01:18 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Jakub Malhotra
-// Created on: Febuary 2023
+// Created on: May 2023
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-jakub-malhotra/sw.js", {
+    scope: "/ICS2O-Unit-1-08-jakub-malhotra/",
+  })
+}
+
+/**
+ * This function displays an alert.
+ */
 function myButtonClicked() {
   alert("Hello, World!")
 }
